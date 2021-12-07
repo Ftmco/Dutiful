23f6f887a8800b43fcb8b1a79649d0a267f380d6
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-07 21:15:36 +0330

    Create DutifulContext.cs

---
+++ Dutiful/Dutiful.DataBase/Context/DutifulContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dutiful.DataBase.Context;
+
+public class DutifulContext : DbContext
+{
+    public DutifulContext(DbContextOptions<DutifulContext> options) : base(options)
+    { }
+
+
+}
