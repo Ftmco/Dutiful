b34b5e1c0ec9c3005d6e29fa0fa9588019a4fbef
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-08 11:31:37 +0330

    Update DutifulContext.cs

--- Dutiful/Dutiful.DataBase/Context/DutifulContext.cs
+++ Dutiful/Dutiful.DataBase/Context/DutifulContext.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dutiful.Entity.Project;
+using Dutiful.Entity.Team;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dutiful.DataBase.Context;
 
@@ -12,5 +9,13 @@ public class DutifulContext : DbContext
     public DutifulContext(DbContextOptions<DutifulContext> options) : base(options)
     { }
 
+    public virtual DbSet<Team> Team { get; set; }
 
+    public virtual DbSet<Project> Project { get; set; }
+
+    public virtual DbSet<WorkList> WorkList { get; set; }
+
+    public virtual DbSet<Card> Card { get; set; }
+
+    public virtual DbSet<WorkTask> WorkTask { get; set; }
 }
