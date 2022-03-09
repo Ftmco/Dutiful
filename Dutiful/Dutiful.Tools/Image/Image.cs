61096f790f7487a45678e04f7a96a2e9706cc562
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-09 09:06:07 -0800

    Revert "use encrypting api request ad response"
    This reverts commit b4e301ad79ceff914a090d20192e3e420487969f.

---
+++ Dutiful/Dutiful.Tools/Image/Image.cs
@@ -0,0 +1,17 @@
+﻿using Dutiful.ViewModels.Tools;
+
+public static class ImageExtension
+{
+    public static async Task<string> SaveBase64FileAsync(this SaveBase64File saveBase64File)
+        => await Task.Run(async () =>
+        {
+            byte[] fileBytes = Convert.FromBase64String(saveBase64File.Base64);
+            SaveByteFile saveByteFile = new(fileBytes, saveBase64File.Path);
+            return await saveByteFile.SaveByteFileAsync();
+        });
+
+    public static async Task<string> SaveByteFileAsync(this SaveByteFile saveByteFile)
+    {
+        return "";
+    }
+}
