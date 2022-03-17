5d2afebce44c7a770a3f91a42fb236f69c4cbf47
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-17 13:12:00 -0700

    create team and file view models

---
+++ Dutiful/Dutiful.ViewModels/Team/Team.cs
@@ -0,0 +1,14 @@
+﻿using Dutiful.ViewModels.Tools;
+
+namespace Dutiful.ViewModels.Team;
+
+public record TeamViewModel(Guid Id,string Name,string Bio,FileViewModel Avatar);
+
+public record UpsertTeam(Guid? Id,string Name,string Bio,SendFile Avatar);
+
+public record UpsertTeamResponse(TeamActionStatus Status,TeamViewModel Team);
+
+public enum TeamActionStatus
+{
+
+}
---
+++ Dutiful/Dutiful.ViewModels/Tools/File.cs
@@ -0,0 +1,9 @@
+﻿namespace Dutiful.ViewModels.Tools;
+
+public record SaveBase64File(string Base64, string Path);
+
+public record SaveByteFile(byte[] Bytes,string Path);
+
+public record SendFile(string Base64,string OgName,string Type);
+
+public record FileViewModel(Guid Id,string Name,string OgName,string CreateDate,string Directory,string Type,string ObjectType);
