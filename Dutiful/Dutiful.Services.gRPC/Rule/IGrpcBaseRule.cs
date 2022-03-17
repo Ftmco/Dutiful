c6bad2d1b3b6241f2f24e210b838d63f7723c37b
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-17 13:13:08 -0700

    create grpc client servides

---
+++ Dutiful/Dutiful.Services.gRPC/Rule/IGrpcBaseRule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dutiful.Services.gRPC.Rule;
+
+public interface IGrpcBaseRule : IAsyncDisposable
+{
+    Task CreateChannelAsync(string address);
+}
---
+++ Dutiful/Dutiful.Services.gRPC/Service/GrpcBaseService.cs
@@ -0,0 +1,20 @@
+﻿using Dutiful.Services.gRPC.Rule;
+using Grpc.Net.Client;
+using IdentityProto;
+
+
+namespace Dutiful.Services.gRPC.Service;
+
+public class GrpcBaseService : IGrpcBaseRule
+{
+    public Task CreateChannelAsync(string address)
+    {
+        var channel = GrpcChannel.ForAddress(address);
+        return Task.CompletedTask;
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
