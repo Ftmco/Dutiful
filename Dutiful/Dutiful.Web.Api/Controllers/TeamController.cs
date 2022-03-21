b47058d4831ef88b40396d1b9681fb153455e008
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:42:41 -0700

    upsert teams and get teams

--- Dutiful/Dutiful.Web.Api/Controllers/TeamController.cs
+++ Dutiful/Dutiful.Web.Api/Controllers/TeamController.cs
@@ -1,6 +1,5 @@
 ﻿using Dutiful.Services.Rules;
 using Dutiful.ViewModels.Team;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dutiful.Web.Api.Controllers;
@@ -20,14 +19,42 @@ public class TeamController : ControllerBase
     [HttpGet("GetTeams")]
     public async Task<IActionResult> GetTeams()
     {
-         await _team.GetTeamsAsync(HttpContext);
-        return Ok();
+        var teams = await _team.GetTeamsAsync(HttpContext);
+        return teams.Status switch
+        {
+            TeamActionStatus.Success => Ok(Success("", "", teams.Teams)),
+            TeamActionStatus.UserNotFound => Ok(Faild(401, "User Not Authorized", "Please Login And Try Again")),
+            TeamActionStatus.Exception => Ok(ApiException("Exception,Please Try Again", "")),
+            TeamActionStatus.TeamNotFound => throw new NotImplementedException(),
+            _ => Ok(ApiException("Exception,Please Try Again", "")),
+        };
     }
-        
+
     [HttpPost("Upsert")]
     public async Task<IActionResult> Upsert(UpsertTeam upsertTeam)
     {
         UpsertTeamResponse upsert = await _team.UpsertAsync(upsertTeam, HttpContext);
-        return Ok();
+        return upsert.Status switch
+        {
+            TeamActionStatus.Success => Ok(Success($"Action Success Fully For {upsert.Team.Name}", "", upsert.Team)),
+            TeamActionStatus.UserNotFound => Ok(Faild(401, "User Not Authorized", "Please Login And Try Again")),
+            TeamActionStatus.Exception => Ok(ApiException("Exception,Please Try Again", "")),
+            TeamActionStatus.TeamNotFound => Ok(Faild(404, "Team Not Found", "")),
+            _ => Ok(ApiException("Exception,Please Try Again", "")),
+        };
+    }
+
+    [HttpGet("Delete")]
+    public async Task<IActionResult> Delete(string token)
+    {
+        TeamActionStatus delete = await _team.DeleteAsync(token,HttpContext);
+        return delete switch
+        {
+            TeamActionStatus.Success => Ok(Success("Team Deleted", "", new { })),
+            TeamActionStatus.UserNotFound => Ok(Faild(401, "User Not Authorized", "Please Login And Try Again")),
+            TeamActionStatus.Exception => Ok(ApiException("Exception,Please Try Again", "")),
+            TeamActionStatus.TeamNotFound => Ok(Faild(404, "Team Not Found", "")),
+            _ => Ok(ApiException("Exception,Please Try Again", "")),
+        };
     }
 }
