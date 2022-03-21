9a5b8adffb0b342e209510c1033f1f9d532b3101
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:45:33 -0700

    Create User.cs

---
+++ Dutiful/Dutiful.ViewModels/User/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dutiful.ViewModels.User;
+
+public record UserViewModel(Guid Id, string UserName, string FullName, string Email, string MobileNo, bool IsActive, DateTime RegisterDate);
