d57c246d5e75267aab980fe7040b3f85cc7cda0c
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:45:30 -0700

    Update Team.cs

--- Dutiful/Dutiful.ViewModels/Team/Team.cs
+++ Dutiful/Dutiful.ViewModels/Team/Team.cs
@@ -2,13 +2,18 @@
 
 namespace Dutiful.ViewModels.Team;
 
-public record TeamViewModel(Guid Id,string Name,string Bio,FileViewModel Avatar);
+public record TeamViewModel(Guid Id, string Name, string Bio,string Token, FileViewModel Avatar);
 
-public record UpsertTeam(Guid? Id,string Name,string Bio,SendFile Avatar);
+public record UpsertTeam(Guid? Id, string Name, string Bio, SendFile Avatar);
 
-public record UpsertTeamResponse(TeamActionStatus Status,TeamViewModel Team);
+public record UpsertTeamResponse(TeamActionStatus Status, TeamViewModel? Team);
+
+public record GetTeamsResponse(TeamActionStatus Status,IEnumerable<TeamViewModel> Teams);
 
 public enum TeamActionStatus
 {
-
+    Success = 0,
+    UserNotFound = 1,
+    Exception = 2,
+    TeamNotFound = 3
 }
