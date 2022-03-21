2654dee7f4c146faf13b7ae9d38d8f35ecf0e3b7
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:45:27 -0700

    create application models

---
+++ Dutiful/Dutiful.ViewModels/MicroService/Application.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dutiful.ViewModels.MicroService;
+
+public record Application(string ApiKey,string Password);
