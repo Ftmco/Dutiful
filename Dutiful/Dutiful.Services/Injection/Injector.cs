3882cae3e9cba4d2da6f2cb8c136ad2d1b757e71
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:43:29 -0700

    create injection service

---
+++ Dutiful/Dutiful.Services/Injection/Injector.cs
@@ -0,0 +1,39 @@
+﻿using Dutiful.Entity.Team;
+using Dutiful.Services.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Service.Injection;
+
+public static class Injector
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection service)
+    {
+        service.AddBaseDependency();
+        service.AddServiceDepdency();
+        service.AddToolsDependncy();
+        return service;
+    }
+
+    public static IServiceCollection AddBaseDependency(this IServiceCollection service)
+    {
+        service.AddScoped<IBaseRules<Team>, BaseServices<Team>>();
+
+
+        return service;
+    }
+
+    public static IServiceCollection AddServiceDepdency(this IServiceCollection service)
+    {
+        service.AddTransient<IAccountRules, AccountServices>();
+        service.AddTransient<ITeamRules, TeamServices>();
+        service.AddTransient<IApiCall, ApiCall>();
+        service.AddTransient<HttpClient>();
+
+        return service;
+    }
+
+    public static IServiceCollection AddToolsDependncy(this IServiceCollection service)
+    {
+        return service;
+    }
+}
