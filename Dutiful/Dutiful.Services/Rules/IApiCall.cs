e6fc22ae248565e1d8395809fb6703ee3ec75801
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:44:37 -0700

    create api call service for work with enc api call

---
+++ Dutiful/Dutiful.Services/Rules/IApiCall.cs
@@ -0,0 +1,17 @@
+﻿using Dutiful.ViewModels.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dutiful.Services.Rules;
+
+public interface IApiCall : IAsyncDisposable
+{
+    Task<TResponse?> PostAsync<TResponse>(string url, object data);
+
+    Task<TResponse?> PostEncAsync<TResponse>(string url, object data);
+
+    Task SetBaseUrlAsync(string baseUrl);
+}
---
+++ Dutiful/Dutiful.Services/Services/ApiCall.cs
@@ -0,0 +1,48 @@
+﻿using Dutiful.ViewModels.Api;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using Tools.Crypto;
+
+namespace Dutiful.Services.Services;
+
+public class ApiCall : IApiCall
+{
+    readonly HttpClient _httpClient;
+
+    public ApiCall(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+
+    public async Task<TResponse?> PostAsync<TResponse>(string url, object data)
+    {
+        HttpResponseMessage? request = await _httpClient.PostAsJsonAsync(url, data);
+        if (request.IsSuccessStatusCode)
+        {
+            TResponse? response = await request.Content.ReadFromJsonAsync<TResponse>();
+            return response;
+        }
+        return default;
+    }
+
+    public async Task<TResponse?> PostEncAsync<TResponse>(string url, object data)
+    {
+        var key = url.KeyMaker();
+        string? encData = JsonConvert.SerializeObject(data).Encrypt(key);
+        ApiRequest? request = await PostAsync<ApiRequest>(url, new { Data = encData });
+        TResponse? response = JsonConvert.DeserializeObject<TResponse>(request?.Data.Decrypt(key) ?? "");
+        return response;
+    }
+
+    public Task SetBaseUrlAsync(string baseUrl)
+    {
+        _httpClient.BaseAddress = new Uri(baseUrl);
+        return Task.CompletedTask;
+    }
+}
