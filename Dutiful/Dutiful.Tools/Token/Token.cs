1214b23fce6098c55438c2c1ffb3787ba77b2631
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:42:03 -0700

    token generator tools

---
+++ Dutiful/Dutiful.Tools/Token/Token.cs
@@ -0,0 +1,13 @@
+﻿using Tools.Crypto;
+
+namespace Dutiful.Tools.Token;
+
+public static class TokenExtensions
+{
+    public static string CreateToken(this string name)
+    {
+        string guid = Guid.NewGuid().ToString();
+        string hash = guid.CreateSHA256();
+        return $"{name}-{hash}";
+    }
+}
